c72f7fff455c50686921d0c1a6ea28741a0b5573
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-04-27 20:48:24 +0100

    require + module / finale

---
+++ examples/chapitre-02/require.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var fs = require('fs');
+var setupFormatter = require('./currency-format');
+var chap2Index = require('../chapitre-02');
+var _ = require('lodash');
+var packageInfos = require('./package.json');
