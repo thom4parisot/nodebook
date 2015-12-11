207507bc0f1f2d33483939374830efdadc51b19c
oncletom <hi@oncletom.io>
2015-12-11 17:32:08 +0000

    Chapitre 4 : requête HTTP universelle

---
+++ chapter-04/examples/src/universal-http/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+if (typeof require === 'function') {         // <1>
+  var superagent = require('superagent');
+}
+
+superagent
+  .get('http://localhost:4000/src/universal-http/sample.json')
+  .end((err, response) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(response.body.cheeses);      // <2>
+  });
