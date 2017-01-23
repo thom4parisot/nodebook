36f45d03beb6894e3f12b1db9338b6871240e686
Thomas Parisot <hi@oncletom.io>
2017-01-23 18:45:03 +0100

    Template literals et Tagged template literals
    fix #74

---
+++ chapter-02/examples/src/primitives-tagged-template-literals.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { safeHtml } = require('common-tags');
+const text = '<script>alert(document.cookie)</script>';
+
+console.log(`<div class="user-content">
+  ${text}
+</div>`); // <1>
+
+console.log(safeHtml`<div class="user-content">
+  ${text}
+</div>`); // <2>
---
+++ chapter-02/examples/src/primitives-template-literals.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const lang = 'JavaScript';
+const { basename } = require('path');
+
+console.log(`I ♥ ${lang}.
+Cet exemple se trouve dans le fichier "${basename(__filename)}"`); // <1>
