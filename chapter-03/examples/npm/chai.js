c4b5d80cfb01cd27d1a87a2bfd5e502afe257cd4
Thomas Parisot <hi@oncletom.io>
2017-01-27 11:01:21 +0100

    Développement de l'exemple de chai.js en réintroduisant mocha

--- chapter-03/examples/npm/chai.js
+++ chapter-03/examples/npm/chai.js
@@ -1,7 +1,30 @@
 'use strict';
 
+// ./node_modules/.bin/mocha examples/npm/chai.js
+
+/* global describe, it */
+
 const { expect } = require('chai');
+const messageAbbr = require('../message-abbr');
 
-const expectedDate = new Date('1983-03-24').getTime();
+describe('Date assertion sandbox', () => {
+  it('should state than now is greater than my birth date', () => {
+    expect(new Date()).to.be.above(new Date('1983/03/24'));
+  });
+});
 
-expect(Date.now()).to.be.greaterThan(expectedDate);
+describe('messageAbbr', () => {
+  it('should return a capitalised abbr', (done) => {
+    messageAbbr('hello world', (err, abbr) => {
+      expect(err).to.be.null;
+      expect(abbr).to.equal('HW');
+      done();
+    });
+  });
+
+  it('should populate the callback error argument if it fails', () => {
+    messageAbbr(null, (err) => {
+      expect(err).to.be.an(Error);
+    })
+  });
+});
