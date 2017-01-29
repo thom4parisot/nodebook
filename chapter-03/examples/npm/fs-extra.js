d659010750a41a94acefab091f2adabe622050cb
Frank Taillandier <frank.taillandier@gmail.com>
2017-01-29 21:03:53 +0100

    Exemple plus complet

--- chapter-03/examples/npm/fs-extra.js
+++ chapter-03/examples/npm/fs-extra.js
@@ -1,9 +1,26 @@
 'use strict';
 
+const mkdirp = require('mkdirp');
+const rm = require('rimraf');
+const { join } = require('path');
 const fs = require('fs-extra');
 
-fs.copy(__dirname, '/tmp/nodebook-examples', (err) => {
+const dirpath = join(__dirname, '..', '..', 'tmp', 'nodebook-examples');
+
+fs.ensureDir(dirpath, function (err) {
   if (err) {
-    return console.error('Une erreur s\'est produite.', err);
+    fs.mkdirp(dirpath);
+    console.log('Dossier crée :', dirpath);
   }
+})
+
+rm(dirpath, () => {
+  fs.copy(__dirname, dirpath, (err) => {
+    if (err) {
+      return console.error('Une erreur s\'est produite.', err);
+    }
+    else {
+      return console.log('Dossier copié : ', dirpath);
+    }
+  });
 });
