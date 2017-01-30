bb1acb5307621591a7928b767b612e0725feceea
Thomas Parisot <hi@oncletom.io>
2017-01-31 00:09:54 +0100

    Explication de  et de
    fix #10

---
+++ chapter-03/examples/server-crash.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+const { parse } = require('url');
+
+const server = http.createServer((req, res) => {
+  // eslint-disable-next-line no-undef
+  const { query } = parse(reb.url, true); // <1>
+
+  res.end();
+});
+
+server.listen(4001);
+process.on('uncaughtException', err => console.error(err)); // <2>
+
+setTimeout(() => http.get('http://localhost:4001/'), 1000); // <3>
