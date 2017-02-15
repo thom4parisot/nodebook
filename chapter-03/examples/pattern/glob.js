125fe86a1e123d9c6718439416021a1907a475c7
Thomas Parisot <oncletom@users.noreply.github.com>
2017-02-15 17:31:12 +0100

    jade -> pug (#113)

--- chapter-03/examples/pattern/glob.js
+++ chapter-03/examples/pattern/glob.js
@@ -16,5 +16,5 @@ const displayFiles = (err, files) => {
 glob('../template.*', options, displayFiles);        // <1>
 glob('../template.{hbs,ej*}', options, displayFiles);// <2>
 glob('../template.[ej]*', options, displayFiles);    // <3>
-glob('../template.!(jade)', options, displayFiles);  // <4>
+glob('../template.!(pug)', options, displayFiles);  // <4>
 glob('../**/*.css', displayFiles);                   // <5>
