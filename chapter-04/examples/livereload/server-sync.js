e0cf56059390222fb0761a51d0f9686c0d5882e5
Thomas Parisot <hi@oncletom.io>
2017-02-21 15:06:21 +0100

    Chapitre 4 : browser-sync
    refs #72

---
+++ chapter-04/examples/livereload/server-sync.js
@@ -0,0 +1,23 @@
+const browserSync = require('browser-sync');
+
+module.exports = (server) => {             // <1>
+  return port => {                         // <2>
+    const PUBLIC_PORT = 4000;              // <3>
+    const bs = browserSync.create();
+
+    server.listen(port, () => {            // <4>
+      console.log(`Listening on http://localhost:${PUBLIC_PORT}`);
+    });
+
+    bs.init({                              // <5>
+      files: ['./examples'],
+      port: PUBLIC_PORT,
+      open: false,
+      logPrefix: 'nodebook',
+      proxy: {
+        target: `http://localhost:${port}`,// <6>
+        ws: true,
+      }
+    });
+  };
+};
--- chapter-04/server.js
+++ chapter-04/server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const express = require('express');
+const getPort = require('get-port');
 
 const WebsocketRoute = require('./examples/io/websocket-server.js');
 const EventSourceRoute = require('./examples/io/eventsource-server.js');
 const FetchRoute = require('./examples/io/fetch-server.js');
+const serverSync = require('./examples/livereload/server-sync.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,6 +16,6 @@ FetchRoute(app);
 
 app.use('/', express.static('./'));
 
-server.listen(4000, () => {
-  console.log('Listening on http://localhost:4000')
-});
+getPort()
+  .then(serverSync(server))
+  .catch(console.error);
