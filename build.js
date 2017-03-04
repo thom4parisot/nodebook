eefe847d2f3776db83ee53ea689c0f27c87f4cec
Guillaume Grossetie <ggrossetie@gmail.com>
2017-03-04 14:31:08 +0100

    Integrate with Makefile

--- build.js
+++ build.js
@@ -1,28 +1,9 @@
 'use strict'
 
-var fs = require('fs');
 var glob = require('glob');
-
-var removeSync = function(dir) {
-  var files = [];
-  if (fs.existsSync(dir)) {
-    files = fs.readdirSync(dir);
-    files.forEach(function(file){
-      var curPath = dir + '/' + file;
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        removeSync(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(dir);
-  }
-};
 
 var processor = require('asciidoctor.js')();
 var buildDir = 'dist';
-
-removeSync(buildDir);
 
 var defaultAttributes = ['data-uri', 'toc=macro', 'toclevels=4', 'icons=font', 'lang=fr', 'env=ci', 'hide-uri-scheme', 'experimental'];
 glob('{index.adoc,chapter-*/index.adoc,foreword/*.adoc,appendix-*/*.adoc}', function (err, files) {
