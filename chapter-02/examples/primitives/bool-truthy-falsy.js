3e36b9e250b157c4272b126047fabd480cce323b
Thomas Parisot <hi@oncletom.io>
2017-03-06 00:43:00 +0100

    Chapitre 2 : Boolean

---
+++ chapter-02/examples/primitives/bool-truthy-falsy.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const counter = 3;
+
+console.log(Boolean(""));       // <1>
+console.log(Boolean(counter));  // <2>
+
+if (counter) {                  // <3>
+  console.log('if (counter) équivaut à if (Boolean(counter))');
+}
---
+++ chapter-02/examples/primitives/bool.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const counter = 3;
+
+console.log(counter);               // <1>
+console.log(counter > 0);           // <2>
+console.log(counter < 0);           // <3>
+console.log(counter === 3);         // <4>
+console.log(counter && counter > 0);// <5>
