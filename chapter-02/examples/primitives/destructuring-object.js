a117accfc25a0af42c51843ddccda155a93e32e0
Thomas Parisot <hi@oncletom.io>
2017-03-06 01:19:06 +0100

    Chapitre 2 : dernières corrections de décomposition etc.

--- chapter-02/examples/primitives/destructuring-object.js
+++ chapter-02/examples/primitives/destructuring-object.js
@@ -7,14 +7,14 @@ const config = {
   debug: true,
 };
 
-const { debug } = config;
+const { debug: configDebug } = config;
 const { isProd, name = 'Demo' } = config;
 const logConfigDebug = ({ debug }) => {           // <2>
   console.log(debug);
 };
 
 console.log(join('folder', 'filename.txt'));      // <3>
-console.log(debug);     // <4>
-console.log(isProd);    // <5>
-console.log(name);      // <6>
+console.log(configDebug); // <4>
+console.log(isProd);      // <5>
+console.log(name);        // <6>
 logConfigDebug(config, 'zero', 'un', 'deux');     // <7>
