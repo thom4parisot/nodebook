0a820a4c538cb29c39e8f024b0c24a80dd36f6dc
Thomas Parisot <hi@oncletom.io>
2017-03-07 11:32:58 +0100

    Chapitre 6 : NODE_ENV
    fix #135

--- chapter-06/examples/config/env.js
+++ chapter-06/examples/config/env.js
@@ -3,6 +3,8 @@
 const server = require('http').createServer();
 const defaultPort = process.env.NODE_ENV === 'test' ? 3001 : 3000;
 
-// …
+const {PORT=defaultPort} = process.env; // <1>
 
-server.listen(process.env.PORT || defaultPort);
+server.listen(PORT, () => {
+  console.log(`En écoute sur http://localhost:${PORT}`);
+});
