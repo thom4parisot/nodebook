a7d355dcf300e9774b5e870d0643a4c7ea4b4ca9
Thomas Parisot <hi@oncletom.io>
2018-01-01 14:26:20 +0100

    fs-extra retourne des promesses par défaut
    Pas besoin d'utiliser le module pify ni util.promisify

--- appendix-a/examples/npm/fs-extra.js
+++ appendix-a/examples/npm/fs-extra.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const { join } = require('path');
-const pify = require('pify');
-const fs = pify(require('fs-extra'));
+const {join} = require('path');
+const fs = require('fs-extra');
+const {log, error} = console;
 
 const tmp = join(__dirname, '..', '..', 'tmp');
 const dirpath = join(tmp, 'nodebook-examples');
 
 Promise.resolve(tmp)
   .then(tmp => fs.ensureDir(tmp))
-  .then((result) => result && console.log('Dossier créé : %s', result))
+  .then((result) => result && log('Dossier créé : %s', result))
   .then(() => fs.remove(dirpath))
   .then(() => fs.copy(__dirname, dirpath))
-  .then(() => console.log('Dossier copié : %s', dirpath))
-  .catch(err => console.error('Une erreur s\'est produite.', err));
+  .then(() => log('Dossier copié : %s', dirpath))
+  .catch(err => error('Une erreur s\'est produite.', err));
