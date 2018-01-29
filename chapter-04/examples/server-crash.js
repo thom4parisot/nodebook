fdf07a2cd864f5cf75ab02de1abe6ecfac841718
Thomas Parisot <hi@oncletom.io>
2018-01-29 06:20:08 +0100

    _Chapitre 4_ : ajustement d'exemples pour renforcer les objectifs d'apprentissage

--- chapter-04/examples/server-crash.js
+++ chapter-04/examples/server-crash.js
@@ -3,14 +3,14 @@
 const http = require('http');
 const {parse} = require('url');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, response) => {
   // eslint-disable-next-line
-  const {query} = parse(reb.url, true); // <1>
+  const url = parse(requete.url, true); // <1>
 
-  res.end();
+  response.end(JSON.stringify(url, null, 2));
 });
 
 server.listen(4001);
-process.on('uncaughtException', err => console.error(err)); // <2>
+process.on('uncaughtException', console.error);             // <2>
 
 setTimeout(() => http.get('http://localhost:4001/'), 1000); // <3>
