05cbe2204ab355647fa9f983df70f221f3d9eab4
Thomas Parisot <hi@oncletom.io>
2018-02-03 19:39:55 +0100

    Chapitre 3 : correction suite à un oubli de renvoi

--- chapter-03/examples/regexp/flags.js
+++ chapter-03/examples/regexp/flags.js
@@ -1,5 +1,5 @@
 const text = 'I ♥ Node.js & Anode';
 
 console.log(text.match(/node/i));   // <1>
-console.log(text.match(/ode/g));
-console.log(text.match(/node/ig));  // <2>
+console.log(text.match(/ode/g));    // <2>
+console.log(text.match(/node/ig));  // <3>
