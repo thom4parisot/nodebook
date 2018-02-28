679431401139e6135cee135022754e69e2343ac2
Michel <michelc@gmail.com>
2018-02-28 14:23:59 +0100

    Correction guillemets simple en guillemets double. Fixes #226

--- chapter-03/examples/string/base.js
+++ chapter-03/examples/string/base.js
@@ -2,7 +2,7 @@
 console.log('L\'après-midi\nLe soir');  // <1>
 
 // Utilisation de guillemets doubles
-console.log('L\'après-midi\nLe soir');   // <2>
+console.log("L'après-midi\nLe soir");   // <2>
 
 // Utilisation de guillemets obliques
 console.log(`L'après-midi
