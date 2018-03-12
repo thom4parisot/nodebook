b3988e7fb0aeea9f7d6e95a4507c01eca73a402c
Thomas Parisot <hi@oncletom.io>
2018-03-12 23:18:52 +0000

    Outillage : stylage des blocs

--- src/asciidoctor-custom-styles.js
+++ src/asciidoctor-custom-styles.js
@@ -8,13 +8,28 @@ module.exports = function HTMLStyles () {
   font-style: normal;
   font-weight: bold;
 }
+
+.admonitionblock td.content > .title, .audioblock > .title, .exampleblock > .title, .imageblock > .title, .listingblock > .title, .literalblock > .title, .stemblock > .title, .openblock > .title, .paragraph > .title, .quoteblock > .title, table.tableblock > .title, .verseblock > .title, .videoblock > .title, .dlist > .title, .olist > .title, .ulist > .title, .qlist > .title, .hdlist > .title {
+  font-size: 1.0625rem;
+}
+
 a {
   white-space: nowrap;
 }
 .RemarquePreTitre {
-	font-style: normal;
+  background-color: #ffc;
+  border-bottom: 1px solid #ddddd8;
+  font-style: normal;
   font-weight: bold;
-	border-bottom: 1px solid #ddddd8;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+.admonitionblock {
+}
+.admonitionblock table {
+  box-shadow: 0 0 10px rgba(0,0,0,.1);
+  margin: 1.5em 0;
+  padding: 1em;
 }
 .icon .title {
   font-size: 2em;
