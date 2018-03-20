604563ab842e4c2eefdb73a59b865514b57ac658
Thomas Parisot <hi@oncletom.io>
2018-03-20 08:52:56 +0000

    Chapitre 3 : rectification de l'exemple sur l'accesseur
    refs #260

--- chapter-03/examples/class/getters.js
+++ chapter-03/examples/class/getters.js
@@ -4,12 +4,12 @@ class Book {
     this.ean13 = ean13;
   }
 
-  get isbn() {
+  get isbn() {                // <1>
     return this.ean13.slice(3);
   }
 }
 
 const nodebook = new Book({ title: 'Node.js', ean13: '9782212139938' });
 
-console.log(nodebook.ean13);  // <1>
-console.log(nodebook.isbn);   // <2>
+console.log(nodebook.ean13);  // <2>
+console.log(nodebook.isbn);   // <3>
