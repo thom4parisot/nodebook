5c02eaf7c98b5d4764de7fe99233d73b0e86c57c
Thomas Parisot <hi@oncletom.io>
2018-04-12 18:07:36 +0200

    Fix : versions de Node et quelques colist

--- chapter-03/examples/regexp/classes.js
+++ chapter-03/examples/regexp/classes.js
@@ -1,7 +1,5 @@
 const text = 'I ♥ RegExp in 2018';
-
-console.log(text.match(/\u{2665} (\w+)/u));     // <1>
+console.log(text.match(/\u{2665} (\w+)/u));   // <1>
 
 const [,iLove,year] = text.match(/^(I \u{2665}).+(\d{4})$/u);
-
-console.log(`${iLove} ${year}`);                // <2>
+console.log(`${iLove} ${year}`);              // <2>
