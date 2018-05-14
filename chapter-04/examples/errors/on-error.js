82c429b05f40fee75be323f227f9ab373dc346aa
Thomas Parisot <oncletom@users.noreply.github.com>
2018-05-14 07:14:11 +0200

    Relecture : seconde passe, suppression des … et ajout de points sur chaque colist

--- chapter-04/examples/errors/on-error.js
+++ chapter-04/examples/errors/on-error.js
@@ -1,5 +1,5 @@
 'use strict';
 
-process.on('error', (error) => console.error(error));   // <2>
-
 process.emit('error', new Error('Oops !'));             // <1>
+
+process.on('error', (error) => console.error(error));   // <2>
--- chapter-04/examples/events/remove.js
+++ chapter-04/examples/events/remove.js
@@ -1,20 +1,20 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const tick = () => process.stdout.write('.');
+let counter = 0;
 
-const log = (date) => {
-  console.log(
-    'Il est %s:%s:%s',
-    date.getUTCHours(),
-    date.getUTCMinutes(),
-    date.getSeconds()
-  );
-};
+setInterval(() => {
+  counter++;
+  emitter.emit('date', new Date());             // <1>
 
-setInterval((counter) => {
-  console.log('tic');
-  emitter.emit('date', new Date(), counter++);              // <1>
-  counter === 3 ? emitter.removeListener('date', log) : ''; // <2>
-  counter === 5 ? process.exit(0) : '';
-}, 1000, 0);
+  if (counter === 5) {
+    process.exit(0);
+  }
+}, 1000);
 
-emitter.on('date', log);
+emitter.on('date', tick);                       // <2>
+emitter.on('date', () => {
+  if (counter === 3) {
+    emitter.removeListener('date', tick);       // <3>
+  }
+});
