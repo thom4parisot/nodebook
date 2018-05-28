8b6c58c4cfed0cdc1ff47660f2e5adf3c8cd5b82
Thomas Parisot <oncletom@users.noreply.github.com>
2018-05-28 20:00:57 +0200

    Annexe A : mise à jour des dépendances suite à un autir de sécurité

--- appendix-a/examples/npm/restify.js
+++ appendix-a/examples/npm/restify.js
@@ -1,24 +1,22 @@
 'use strict';
 
 const restify = require('restify');
+const {NotFoundError} = require('restify-errors');
 const server = restify.createServer();
 const countries = require('i18n-iso-countries');
 
-const codes = countries.getNames('en');
-
-const validate = (request, response, next) => {
-  return (request.params.code in codes)
-    ? next()
-    : next(new restify.errors.NotFoundError('Country not found'));
-};
-
 const responseV1 = (request, response, next) => {
+  const codes = countries.getNames('en');
+
+  if (codes[request.params.code] === undefined) {
+    return next(new NotFoundError('Country not found'));
+  }
+
   response.send(200, codes[request.params.code]);
 
   next();
 };
 
-server.use(restify.CORS());
-server.get({ path: '/countries/:code', version: '1.0.0' }, validate, responseV1);
+server.get({ path: '/countries/:code', version: '1.0.0' }, responseV1);
 
 server.listen(8080, () => console.log('Serveur accessible sur %s', server.url));
