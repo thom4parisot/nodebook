b9b27481e3c851f06944be5acf50da3802ef8742
Thomas Parisot <oncletom@users.noreply.github.com>
2018-06-20 15:57:09 +0200

    Annexe A : suppression de modules dont je parle déjà au chapitre 8

--- appendix-a/examples/npm/chalk.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const chalk = require('chalk');
-
-const Blue = chalk.blue;
-const White = chalk.white;
-const Red = chalk.red;
-
-console.log(Blue('Node.js'), White('c\'est'), Red('fantastique !'));
