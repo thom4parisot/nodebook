4318368e20b19f9b43c0bbf7d2c6554c9a8208f0
Thomas Parisot <oncletom@users.noreply.github.com>
2018-07-23 00:57:00 +0200

    Chapitre 9 : exemple affiché en plusieurs morceaux
    closes #312

--- chapter-09/examples/tests/date-interval.js
+++ chapter-09/examples/tests/date-interval.js
@@ -6,19 +6,19 @@ import DateInterval from '../modules/date-interval.jsx';// <1>
 describe('<DateInterval />', () => {
   const element = createElement(DateInterval);
 
-  it('affiche un élément <time>', () => {               // <2>
-    const component = shallow(element);                 // <3>
+  it('affiche un élément <time>', () => {               // <1>
+    const component = shallow(element);                 // <2>
 
-    expect(component.find('time')).to.have.length(1);   // <4>
+    expect(component.find('time')).to.have.length(1);   // <3>
   });
 
   it('la propriété tickDate influence la classe HTML', () => {
     const OK_CLASS = ['pair', 'impair'];
-    const component = shallow(element);                 // <5>
-    const {tickData} = component.state();               // <6>
+    const component = shallow(element);                 // <1>
+    const {tickData} = component.state();               // <2>
 
     const time = tickData.now.getTime();
-    expect(time).to.be.closeTo(Date.now(), 3);          // <7>
-    expect(tickData.className).to.be.oneOf(OK_CLASS);   // <8>
+    expect(time).to.be.closeTo(Date.now(), 3);          // <3>
+    expect(tickData.className).to.be.oneOf(OK_CLASS);   // <4>
   });
 });
